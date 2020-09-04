df2eecbddcfa480a1eee79d40153376461b211c6
datadiskpfv <paul.valle@datadisk.co.uk>
2020-09-04 14:09:42 +0100

    working on tests for AuthorityService

--- src/test/java/uk/co/datadisk/ddrolodex/services/AuthorityServiceTest.java
+++ src/test/java/uk/co/datadisk/ddrolodex/services/AuthorityServiceTest.java
@@ -2,17 +2,39 @@ package uk.co.datadisk.ddrolodex.services;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.co.datadisk.ddrolodex.domain.security.Authority;
+import uk.co.datadisk.ddrolodex.repositories.security.AuthorityRepository;
+import uk.co.datadisk.ddrolodex.services.impl.AuthorityServiceImpl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+//@WebMvcTest
 class AuthorityServiceTest {
+
+    @MockBean
+    AuthorityRepository authorityRepository;
 
     @Autowired
     private AuthorityService authorityService;
 
+    List<Authority> authorities;
+
     @BeforeEach
     void setUp() {
+        authorities = new ArrayList<>(Arrays.asList(Authority.builder().permission("user.create").build()));
     }
 
     @Test
@@ -25,10 +47,19 @@ class AuthorityServiceTest {
 
     @Test
     void findAuthorityByName() {
+        Authority authority = Authority.builder().permission("user.create").build();
+        when(authorityRepository.findAuthorityByPermission("user.create")).thenReturn(Optional.ofNullable((authority)));
+
+        assertEquals("user.create", authorityService.findAuthorityByName("user.create").getPermission());
+        verify(authorityRepository, times(1)).findAuthorityByPermission("user.create");
     }
 
     @Test
     void findAll() {
+        when(authorityRepository.findAll()).thenReturn((authorities));
+        assertEquals(1, authorityService.findAll().size());
+
+        verify(authorityRepository, times(1)).findAll();
     }
 
     @Test
